30ba9c578b2f5047a8c2c66ccb87e5c5d60994d4
yanghao <24621890@qq.com>
2022-03-03 13:23:50 +0800

    del logmanager,merge func to logsegmentset.

--- storage/src/main/java/com/tsingj/sloth/store/log/LogSegmentSet.java
+++ storage/src/main/java/com/tsingj/sloth/store/log/LogSegmentSet.java
@@ -1,24 +1,36 @@
 package com.tsingj.sloth.store.log;
 
 
+import com.tsingj.sloth.store.constants.LogConstants;
 import com.tsingj.sloth.store.properties.StorageProperties;
 import com.tsingj.sloth.store.utils.CommonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.IntervalTask;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Collectors;
 
 /**
  * @author yanghao
+ * logsegment集合
+ * 1、服务启动时完成topic-partition历史数据加载
+ * 2、服务启动后按照topic-partition管理logsegment
  */
+@EnableScheduling
 @Component
-public class LogSegmentSet {
+public class LogSegmentSet implements SchedulingConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(LogSegmentSet.class);
 
@@ -92,5 +104,127 @@ public class LogSegmentSet {
 
     public ConcurrentHashMap<String, ConcurrentSkipListMap<Long, LogSegment>> getLogSegmentsMapping() {
         return this.DATA_LOGFILE_MAP;
+    }
+
+
+
+    /**
+     * 项目启动时触发
+     * -- 加载dataPath目录下所有segment文件和index文件。
+     */
+    @PostConstruct
+    public void loadLogs() {
+        String dataPath = storageProperties.getDataPath();
+        try {
+            File dataDir = new File(dataPath);
+            File[] topicFiles = dataDir.listFiles();
+            if (topicFiles == null || topicFiles.length == 0) {
+                logger.info("dataPath:{} is empty dir, skip initialization.", dataPath);
+                return;
+            }
+            //initialization segments file memory mapping.
+            for (File topicDir : topicFiles) {
+                String topic = topicDir.getName();
+                File[] partitionDirs = topicDir.listFiles();
+                if (partitionDirs == null || partitionDirs.length == 0) {
+                    continue;
+                }
+                for (File partitionDir : partitionDirs) {
+                    long partition = Integer.parseInt(partitionDir.getName());
+                    File[] segmentFiles = partitionDir.listFiles();
+                    if (segmentFiles == null || segmentFiles.length == 0) {
+                        continue;
+                    }
+                    List<File> segmentFileSortedList = Arrays.stream(segmentFiles).sorted(Comparator.comparing(File::getName)).collect(Collectors.toList());
+                    for (File segmentFile : segmentFileSortedList) {
+                        String segmentFileName = segmentFile.getName();
+                        if (segmentFileName.endsWith(LogConstants.FileSuffix.LOG)) {
+                            logger.info("prepare init topic:{} partition:{} segment:{}", topic, partition, segmentFileName);
+                            LogSegment logSegment = LogSegment.loadLogs(segmentFile, storageProperties.getSegmentMaxFileSize(), storageProperties.getLogIndexIntervalBytes());
+                            this.addLogSegment(topic, partition, logSegment);
+                        }
+                    }
+                }
+            }
+            logger.info("-------------------------------------------------load segments over----------------------------------------------------------------");
+        } catch (Throwable e) {
+            logger.error("log recovery fail, please check your log!", e);
+            throw new LogRecoveryException(e);
+        }
+    }
+
+
+    @PreDestroy
+    public void shutdown() {
+        logger.trace("storage prepare destroy.");
+        showIndexCacheStats();
+        flushDirtyLogs();
+        logger.trace("storage destroy done.");
+    }
+
+    /**
+     * 输出indexCacheStats
+     */
+    private void showIndexCacheStats() {
+        ConcurrentHashMap<String, ConcurrentSkipListMap<Long, LogSegment>> logSegmentsMapping = this.getLogSegmentsMapping();
+        if (logSegmentsMapping.isEmpty()) {
+            return;
+        }
+        for (Map.Entry<String, ConcurrentSkipListMap<Long, LogSegment>> entry : logSegmentsMapping.entrySet()) {
+            ConcurrentSkipListMap<Long, LogSegment> logSegmentSkipListMap = entry.getValue();
+            if (logSegmentSkipListMap.isEmpty()) {
+                continue;
+            }
+            Collection<LogSegment> logSegments = logSegmentSkipListMap.values();
+            for (LogSegment logSegment : logSegments) {
+                logSegment.getOffsetIndex().showIndexCacheStats();
+            }
+        }
+    }
+
+
+    /**
+     * 定时刷新日志至磁盘
+     */
+    public void flushDirtyLogs() {
+        logger.debug("prepare flush dirtyLogs.");
+        ConcurrentHashMap<String, ConcurrentSkipListMap<Long, LogSegment>> logSegmentsMapping = this.getLogSegmentsMapping();
+        if (logSegmentsMapping.isEmpty()) {
+            return;
+        }
+        for (Map.Entry<String, ConcurrentSkipListMap<Long, LogSegment>> entry : logSegmentsMapping.entrySet()) {
+            ConcurrentSkipListMap<Long, LogSegment> logSegmentSkipListMap = entry.getValue();
+            if (logSegmentSkipListMap.isEmpty()) {
+                continue;
+            }
+            Collection<LogSegment> logSegments = logSegmentSkipListMap.values();
+            for (LogSegment logSegment : logSegments) {
+                logSegment.flush();
+                logSegment.getOffsetIndex().flush();
+                logSegment.getOffsetIndex().freeNoWarmIndexCache();
+                logSegment.getTimeIndex().flush();
+            }
+        }
+        logger.debug("flush dirtyLogs done.");
+    }
+
+
+    /**
+     * 历史segment清理
+     */
+    public void cleanupLogs() {
+        logger.info("cleanupLogs");
+    }
+
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        logger.info("add flushDirtyLogsTask interval {}ms.", storageProperties.getLogFlushInterval());
+        IntervalTask flushDirtyLogsTask = new IntervalTask(this::flushDirtyLogs, storageProperties.getLogFlushInterval(), 0);
+        taskRegistrar.addFixedDelayTask(flushDirtyLogsTask);
+
+        logger.info("add cleanupLogs interval {}s.", storageProperties.getLogCleanupInterval() / 1000);
+        IntervalTask cleanupLogsTask = new IntervalTask(this::cleanupLogs, storageProperties.getLogCleanupInterval(), 0);
+        taskRegistrar.addFixedDelayTask(cleanupLogsTask);
     }
 }
