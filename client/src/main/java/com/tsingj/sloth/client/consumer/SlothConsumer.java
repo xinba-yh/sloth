869fdf07baa9aa01e22aebd512ba23e4ef198ff1
yanghao <24621890@qq.com>
2022-03-30 21:34:40 +0800

    tmp commit;

--- client/src/main/java/com/tsingj/sloth/client/consumer/SlothConsumer.java
+++ client/src/main/java/com/tsingj/sloth/client/consumer/SlothConsumer.java
@@ -12,7 +12,6 @@ import com.tsingj.sloth.remoting.protocol.ProtocolConstants;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -51,18 +50,19 @@ public class SlothConsumer extends SlothClient {
         this.clientId = UUID.randomUUID().toString();
         this.topic = this.consumerProperties.getTopic();
         this.groupName = this.consumerProperties.getGroupName();
-        //1、立即发送一次heartbeat，并与建立clientId与channel的绑定关系（client - server）。
-        this.heartbeat();
-        //2、获取应该消费的partition。
-        List<Integer> topicPartitions = this.topicPartitionReBalanceRequest(this.consumerProperties);
-        //3、TopicPartitionConsumerManager
+        //1.1、立即发送一次heartbeat，并与建立clientId与channel的绑定关系（client - server）。
+        //1.2、响应分配好的partition
+        List<Integer> topicPartitions = this.heartbeat(groupName, topic, clientId);
+        //2、TopicPartitionConsumerManager
         if (topicPartitions != null && topicPartitions.size() > 0) {
             for (Integer topicPartition : topicPartitions) {
-                TopicPartitionConsumer topicPartitionConsumer = new TopicPartitionConsumer(this.topic, topicPartition);
+                TopicPartitionConsumer topicPartitionConsumer = new TopicPartitionConsumer(this.groupName, this.topic, topicPartition);
                 new Thread(topicPartitionConsumer).start();
                 topicPartitionConsumerMap.put(topicPartition, topicPartitionConsumer);
             }
         }
+        //3、启动心跳线程
+
         log.info("sloth topic {} consumer {} partitions {} init done.", this.topic, clientId, topicPartitions);
     }
 
@@ -74,18 +74,55 @@ public class SlothConsumer extends SlothClient {
 
     //----------------------------------------------
 
+    public List<Integer> heartbeat(String groupName, String topic, String clientId) {
+        long currentCorrelationId = RemoteCorrelationManager.CORRELATION_ID.getAndAdd(1);
+        ResponseFuture responseFuture = new ResponseFuture(currentCorrelationId, this.clientProperties.getConnect().getOnceTalkTimeout());
+        //add 关联关系，handler或者超时的定时任务将会清理。
+        RemoteCorrelationManager.CORRELATION_ID_RESPONSE_MAP.put(currentCorrelationId, responseFuture);
+        try {
+            Remoting.ConsumerHeartbeatRequest consumerHeartbeatRequest = Remoting.ConsumerHeartbeatRequest.newBuilder()
+                    .setGroupName(groupName)
+                    .setTopic(topic)
+                    .setClientId(clientId)
+                    .build();
+            DataPackage dataPackage = DataPackage.builder()
+                    .magicCode(ProtocolConstants.MAGIC_CODE)
+                    .version(ProtocolConstants.VERSION)
+                    .command(ProtocolConstants.Command.CONSUMER_GROUP_HEARTBEAT)
+                    .requestType(ProtocolConstants.RequestType.SYNC)
+                    .timestamp(System.currentTimeMillis())
+                    .data(consumerHeartbeatRequest.toByteArray())
+                    .build();
 
-    private List<Integer> topicPartitionReBalanceRequest(ConsumerProperties clientProperties) {
+            //send data
+            this.getChannel().writeAndFlush(dataPackage);
+
+            DataPackage responseData = responseFuture.waitResponse();
+            if (responseData == null) {
+                log.warn("correlationId {} CONSUMER_GROUP_HEARTBEAT wait response null!", currentCorrelationId);
+                return null;
+            }
+            byte[] data = responseData.getData();
+            Remoting.ConsumerHeartbeatResult consumerHeartbeatResult = Remoting.ConsumerHeartbeatResult.parseFrom(data);
+            if (consumerHeartbeatResult.getRetCode() == Remoting.RetCode.ERROR) {
+                log.warn("correlationId {} CONSUMER_GROUP_HEARTBEAT got error! {} ", currentCorrelationId, consumerHeartbeatResult.getErrorInfo());
+                return null;
+            }
+
+            return consumerHeartbeatResult.getPartitionsList();
+        } catch (InterruptedException e) {
+            log.warn("CONSUMER_GROUP_HEARTBEAT interrupted exception!" + e.getMessage());
+        } catch (InvalidProtocolBufferException e) {
+            log.warn("CONSUMER_GROUP_HEARTBEAT protobuf parse error!" + e.getMessage());
+        } finally {
+            RemoteCorrelationManager.CORRELATION_ID_RESPONSE_MAP.remove(currentCorrelationId);
+        }
         return null;
     }
 
-    private void heartbeat() {
-
-    }
-
     private void reBalanceTopicPartition() {
-        List<Integer> topicPartitions = this.topicPartitionReBalanceRequest(this.consumerProperties);
-        Set<Integer> currentTopicPartitions = topicPartitionConsumerMap.keySet();
+//        List<Integer> topicPartitions = this.topicPartitionReBalanceRequest(this.consumerProperties);
+//        Set<Integer> currentTopicPartitions = topicPartitionConsumerMap.keySet();
         //1.1、获取已经不需要消费的partition。
 
         //1.2、停止不需要消费的partition
@@ -96,46 +133,56 @@ public class SlothConsumer extends SlothClient {
 
     }
 
-    public void getConsumerOffset(int topicPartition) {
-//        long currentCorrelationId = RemoteCorrelationManager.CORRELATION_ID.getAndAdd(1);
-//        ResponseFuture responseFuture = new ResponseFuture(currentCorrelationId, this.clientProperties.getConnect().getOnceTalkTimeout());
-//        //add 关联关系，handler或者超时的定时任务将会清理。
-//        RemoteCorrelationManager.CORRELATION_ID_RESPONSE_MAP.put(currentCorrelationId, responseFuture);
-//        try {
-//            DataPackage dataPackage = DataPackage.builder()
-//                    .magicCode(ProtocolConstants.MAGIC_CODE)
-//                    .version(ProtocolConstants.VERSION)
-//                    .command(ProtocolConstants.Command.SEND_MESSAGE)
-//                    .requestType(ProtocolConstants.RequestType.SYNC)
-//                    .correlationId(currentCorrelationId)
-//                    .timestamp(System.currentTimeMillis())
-//                    .data(message.toByteArray())
-//                    .build();
-//
-//            //send data
-//            this.channel.writeAndFlush(dataPackage);
-//
-//            DataPackage responseData = responseFuture.waitResponse();
-//            if (responseData == null) {
-//                log.warn("correlationId {} wait response null!", currentCorrelationId);
-//                return Remoting.SendResult.newBuilder().setRetCode(Remoting.SendResult.RetCode.ERROR).setErrorInfo("receive data null!").build();
-//            }
-//            byte[] data = responseData.getData();
-//            return Remoting.SendResult.parseFrom(data);
-//        } catch (InterruptedException e) {
-//            return Remoting.SendResult.newBuilder().setRetCode(Remoting.SendResult.RetCode.TIMEOUT).build();
-//        } catch (InvalidProtocolBufferException e) {
-//            return Remoting.SendResult.newBuilder().setRetCode(Remoting.SendResult.RetCode.ERROR).setErrorInfo("protobuf parse error!" + e.getMessage()).build();
-//        } finally {
-//            RemoteCorrelationManager.CORRELATION_ID_RESPONSE_MAP.remove(currentCorrelationId);
-//        }
+    public Long getConsumerOffset(String groupName, String topic, int topicPartition) {
+        long currentCorrelationId = RemoteCorrelationManager.CORRELATION_ID.getAndAdd(1);
+        ResponseFuture responseFuture = new ResponseFuture(currentCorrelationId, this.clientProperties.getConnect().getOnceTalkTimeout());
+        //add 关联关系，handler或者超时的定时任务将会清理。
+        RemoteCorrelationManager.CORRELATION_ID_RESPONSE_MAP.put(currentCorrelationId, responseFuture);
+        try {
+            Remoting.GetConsumerOffsetRequest consumerHeartbeatRequest = Remoting.GetConsumerOffsetRequest.newBuilder()
+                    .setGroupName(groupName)
+                    .setTopic(topic)
+                    .setPartition(topicPartition)
+                    .build();
+            DataPackage dataPackage = DataPackage.builder()
+                    .magicCode(ProtocolConstants.MAGIC_CODE)
+                    .version(ProtocolConstants.VERSION)
+                    .command(ProtocolConstants.Command.GET_CONSUMER_GROUP_OFFSET)
+                    .requestType(ProtocolConstants.RequestType.SYNC)
+                    .timestamp(System.currentTimeMillis())
+                    .data(consumerHeartbeatRequest.toByteArray())
+                    .build();
+
+            //send data
+            this.getChannel().writeAndFlush(dataPackage);
+
+            DataPackage responseData = responseFuture.waitResponse();
+            if (responseData == null) {
+                log.warn("correlationId {} GET_CONSUMER_GROUP_OFFSET wait response null!", currentCorrelationId);
+                return null;
+            }
+            byte[] data = responseData.getData();
+            Remoting.GetConsumerOffsetResult getConsumerOffsetResult = Remoting.GetConsumerOffsetResult.parseFrom(data);
+            if (getConsumerOffsetResult.getRetCode() == Remoting.RetCode.ERROR) {
+                log.warn("correlationId {} GET_CONSUMER_GROUP_OFFSET got error! {} ", currentCorrelationId, getConsumerOffsetResult.getErrorInfo());
+                return null;
+            }
+            return getConsumerOffsetResult.getOffset();
+        } catch (InterruptedException e) {
+            log.warn("GET_CONSUMER_GROUP_OFFSET interrupted exception!" + e.getMessage());
+        } catch (InvalidProtocolBufferException e) {
+            log.warn("GET_CONSUMER_GROUP_OFFSET protobuf parse error!" + e.getMessage());
+        } finally {
+            RemoteCorrelationManager.CORRELATION_ID_RESPONSE_MAP.remove(currentCorrelationId);
+        }
+        return null;
     }
 
-    public void consumerMessage(long offset) {
+    public void consumerMessage(String groupName, String topic, long offset) {
 
     }
 
-    public void submitOffset(long offset) {
+    public void submitOffset(String groupName, String topic, long offset) {
 
     }
 
--- client/src/main/java/com/tsingj/sloth/client/consumer/TopicPartitionConsumer.java
+++ client/src/main/java/com/tsingj/sloth/client/consumer/TopicPartitionConsumer.java
@@ -9,6 +9,8 @@ public class TopicPartitionConsumer implements Runnable {
 
     private final String topic;
 
+    private final String groupName;
+
     private final Integer topicPartition;
 
     private volatile long currentOffset;
@@ -18,38 +20,46 @@ public class TopicPartitionConsumer implements Runnable {
     private volatile boolean running = true;
 
 
-    public TopicPartitionConsumer(String topic, Integer topicPartition) {
+    public TopicPartitionConsumer(String groupName, String topic, Integer topicPartition) {
+        this.groupName = groupName;
         this.topic = topic;
         this.topicPartition = topicPartition;
     }
 
     @Override
     public void run() {
-        //1、询问broker，应该从哪里开始消费
-        SlothConsumer slothConsumer = SlothConsumerManager.getSlothConsumer(topic);
-        Assert.notNull(slothConsumer, "topic:" + topic + " consumer is null!");
-        slothConsumer.getConsumerOffset(topicPartition);
-        this.currentOffset = 0L;
-        //2、开始消费
-        while (running) {
-            long offset = currentOffset + 1;
-            //1、获取消息
-            slothConsumer.consumerMessage(offset);
-            //1.2、根据消息状态判断，如果没有可以消费的消息则休眠一段时间
-            synchronized (this.lock) {
-                try {
-                    this.lock.wait(200);
-                } catch (InterruptedException ignored) {
+        try{
+            //1、询问broker，应该从哪里开始消费
+            SlothConsumer slothConsumer = SlothConsumerManager.getSlothConsumer(topic);
+            Assert.notNull(slothConsumer, "topic:" + topic + " consumer is null!");
+            Long consumerOffset = slothConsumer.getConsumerOffset(groupName, topic, topicPartition);
+            //2、开始消费
+            while (running) {
+                long offset = consumerOffset + 1;
+                //1、获取消息
+                slothConsumer.consumerMessage(groupName, topic, offset);
+                //1.2、根据消息状态判断，如果没有可以消费的消息则休眠一段时间
+                synchronized (this.lock) {
+                    try {
+                        this.lock.wait(200);
+                    } catch (InterruptedException ignored) {
+                    }
                 }
+                //2、触发回调
+
+                //3.1、消费状态成功，提交消息Offset
+                slothConsumer.submitOffset(groupName, topic, offset);
+                this.currentOffset = offset;
+                //3.2、todo 消费状态失败。
+
             }
-            //2、触发回调
-
-            //3.1、消费状态成功，提交消息Offset
-            slothConsumer.submitOffset(offset);
-            currentOffset++;
-            //3.2、todo 消费状态失败。
-
+        }catch (Throwable e){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {
+            }
         }
+
     }
 
     public void stop() {
